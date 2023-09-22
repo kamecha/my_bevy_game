d29abad9640273a44b1f719db28a595f28c99e04
kamecha <masakikameyama81@gmail.com>
2023-09-23 04:27:41 +0900

    一旦画面サイズとか固定したかったけど、無理っぽいな

--- src/main.rs
+++ src/main.rs
@@ -382,6 +382,13 @@ fn main() {
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "test用STGだよ~".to_string(),
+                        resize_constraints: WindowResizeConstraints {
+                            min_width: 1280.0,
+                            min_height: 720.0,
+                            max_width: 1280.0,
+                            max_height: 720.0,
+                        },
+                        resizable: false,
                         ..default()
                     }),
                     ..default()
